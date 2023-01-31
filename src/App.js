df2089c3b34dbdc7e6e3720d162203d2f4c3fd48
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-01-31 21:46:51 +0100

    Finalize Redux ToolKit for counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Counter from "./components/Counter/Counter";
 import Auth from "./components/Auth/Auth";
+import { Provider } from "react-redux";
+import store from "./Store";
 
 function App() {
   return (
     <div className="App">
       <h1>Redux Basic</h1>
-      <Auth />
-      <Counter />
+      <Provider store={store}>
+        <Auth />
+        <Counter />
+      </Provider>
     </div>
   );
 }
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,16 +1,23 @@
 import "./Counter.css";
-
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increase, decrease, toggleShow } from "../../Store/couterSlice";
 
 const Counter = () => {
+  const { counter } = useSelector((state) => state);
+  const dispatch = useDispatch();
   return (
     <div className="Counter">
-      <h2>Count: </h2>
-      <div className="buttons">
-        <button>Increase</button>
-        <button>Decrease</button>
-      </div>
-      <button>Show/Hide Counter</button>
+      {counter.show && (
+        <>
+          <h2>Count: {counter.value} </h2>
+          <div className="buttons">
+            <button onClick={() => dispatch(increase(5))}>Increase</button>
+            <button onClick={() => dispatch(decrease(2))}>Decrease</button>
+          </div>
+        </>
+      )}
+      <button onClick={() => dispatch(toggleShow())}>Show/Hide Counter</button>
     </div>
   );
 };
