d6c9282c6d79f44d2ad245ad62538c26dd20cf81
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-01-31 22:18:06 +0100

    Make the Auth module

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import couterReducer from "./couterSlice";
+import authReducer from "./authSlice";
 
 const store = configureStore({
   reducer: {
     counter: couterReducer,
+    auth: authReducer,
   },
 });
 
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -4,21 +4,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { increase, decrease, toggleShow } from "../../Store/couterSlice";
 
 const Counter = () => {
-  const { counter } = useSelector((state) => state);
+  const { counter, auth } = useSelector((state) => state);
   const dispatch = useDispatch();
   return (
-    <div className="Counter">
-      {counter.show && (
-        <>
-          <h2>Count: {counter.value} </h2>
-          <div className="buttons">
-            <button onClick={() => dispatch(increase(5))}>Increase</button>
-            <button onClick={() => dispatch(decrease(2))}>Decrease</button>
-          </div>
-        </>
+    <>
+      {auth.isLoggedIn && (
+        <div className="Counter">
+          {counter.show && (
+            <>
+              <h2>Count: {counter.value} </h2>
+              <div className="buttons">
+                <button onClick={() => dispatch(increase(5))}>Increase</button>
+                <button onClick={() => dispatch(decrease(2))}>Decrease</button>
+              </div>
+            </>
+          )}
+          <button onClick={() => dispatch(toggleShow())}>
+            Show/Hide Counter
+          </button>
+        </div>
       )}
-      <button onClick={() => dispatch(toggleShow())}>Show/Hide Counter</button>
-    </div>
+    </>
   );
 };
 
